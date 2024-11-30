dc9bc2d195e734bd548e6eeb93cde1bbbab2d934
Islom_the_dev <polzovatelit1@gmail.com>
2024-11-30 15:20:04 +0300

    Update script.js

--- vite-project/script.js
+++ vite-project/script.js
@@ -5,3 +5,28 @@ Card();
 Transactions();
 
 
+
+import { Header } from "../components/Header.js"
+import { users } from "../utils/db.js"
+import { reload } from "../utils/reload.js";
+
+
+console.log(users);
+
+
+const container = document.querySelector('.container')
+console.log(container);
+
+reload(users,Header,container)
+
+
+import  Card  from './component/Card.js';
+import  Transactions  from './component/Transactions.js';
+
+Card();
+Transactions();
+
+
+
+
+
